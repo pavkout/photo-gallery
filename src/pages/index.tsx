3c3bd465c2a06738dd3c28f1b959276d22557485
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-22 10:47:04 +0300

    Initialize next app

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Pavlos Koutoglou</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </div>
+  );
+};
+
+export default HomePage;
