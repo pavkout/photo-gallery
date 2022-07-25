0a8159379875289957b8ddb11257aef57895158f
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-25 14:58:10 +0300

    Create constants for the app

---
+++ src/constants/index.ts
@@ -0,0 +1,66 @@
+type Color = {
+  bg: string;
+  color: string;
+};
+
+/**
+ * These are the colors for albums component.
+ */
+export const albumColors: Color[] = [
+  {
+    bg: '#ECEFF1',
+    color: '#212121',
+  },
+  {
+    bg: '#CFD8DC',
+    color: '#212121',
+  },
+  {
+    bg: '#B0BEC5',
+    color: '#212121',
+  },
+  {
+    bg: '#90A4AE',
+    color: '#212121',
+  },
+  {
+    bg: '#78909C',
+    color: '#FFF',
+  },
+  {
+    bg: '#607D8B',
+    color: '#FFF',
+  },
+  {
+    bg: '#546E7A',
+    color: '#FFF',
+  },
+  {
+    bg: '#455A64',
+    color: '#FFF',
+  },
+  {
+    bg: '#37474F',
+    color: '#FFF',
+  },
+  {
+    bg: '#263238',
+    color: '#FFF',
+  },
+];
+
+/**
+ * This array of colors used by account switcher component.
+ */
+export const accountColors = [
+  '#aec7e8',
+  '#ffbb78',
+  '#98df8a',
+  '#ff9896',
+  '#c5b0d5',
+  '#D6CDEA',
+  '#EFF9DA',
+  '#CDF5F6',
+  '#CBE4F9',
+  '#F9D8D6',
+];
