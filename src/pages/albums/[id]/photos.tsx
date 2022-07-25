91b3923e4b9c85ed9f943e9522ca92ad755e37a6
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-25 15:12:42 +0300

    Create photos page

---
+++ src/pages/albums/[id]/photos.tsx
@@ -0,0 +1,52 @@
+import { GetServerSideProps, NextPage } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import styled from 'styled-components';
+
+// Import components
+import Album from '../../../components/Album';
+
+// Import types
+import type { Photo } from '../../../types';
+
+// Import API
+import { getPhotosByAlbumId } from '../../../utils/api';
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledTitle = styled.h2`
+  color: #757171;
+`;
+
+interface IParams extends ParsedUrlQuery {
+  id: string;
+}
+
+type Props = {
+  photos: Photo[];
+};
+
+const PhotosPage: NextPage<Props> = ({ photos = [] }) => {
+  return (
+    <Wrapper>
+      <StyledTitle>Photos</StyledTitle>
+      <Album photos={photos} />
+    </Wrapper>
+  );
+};
+
+export default PhotosPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params as IParams;
+
+  const photos = await getPhotosByAlbumId(id);
+
+  return { props: { photos } };
+};
