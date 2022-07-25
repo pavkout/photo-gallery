dbac988d66411e930e61eca7cfa757304cab77e1
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-25 23:30:37 +0300

    Change the figure component in order to have the highlighted functionality

--- src/components/Figure/index.tsx
+++ src/components/Figure/index.tsx
@@ -1,5 +1,12 @@
 import React, { ReactNode } from 'react';
 import styled from 'styled-components';
+import { findAll } from 'highlight-words-core';
+
+// Import store
+import { useAppContext } from '../../state/store';
+
+// Import helper function
+import { findCustomChunks } from './helper';
 
 const StyledFigure = styled.figure`
   position: relative;
@@ -28,10 +35,50 @@ export interface FigureProps {
   children: ReactNode;
 }
 
-const Figure = ({ title, children }: FigureProps) => (
-  <StyledFigure>
-    {children}
-    <StyledFigcaption>{title}</StyledFigcaption>
-  </StyledFigure>
-);
+const Figure = ({ title, children }: FigureProps) => {
+  // Use context
+  const { state } = useAppContext();
+
+  // Destract needed state values
+  const { searchingValue } = state;
+
+  // In case the searchingValue is Empty return the text as it is.
+  if (searchingValue === '' || searchingValue.trim() === '') {
+    return (
+      <StyledFigure>
+        {children}
+        <StyledFigcaption>{title}</StyledFigcaption>
+      </StyledFigure>
+    );
+  }
+
+  // Find all chunks with searching value.
+  const chunks = findAll({
+    searchWords: [searchingValue],
+    textToHighlight: title,
+    findChunks: findCustomChunks,
+  });
+
+  // Create the highlighted text from the given chunks.
+  const highlightedText = chunks
+    .map((chunk) => {
+      const { end, highlight, start } = chunk;
+      const text = title.substr(start, end - start);
+
+      if (highlight) {
+        return `<mark><strong><i>${text}</i></strong></mark>`;
+      } else {
+        return text;
+      }
+    })
+    .join('');
+
+  return (
+    <StyledFigure>
+      {children}
+      <StyledFigcaption dangerouslySetInnerHTML={{ __html: highlightedText }} />
+    </StyledFigure>
+  );
+};
+
 export default Figure;
