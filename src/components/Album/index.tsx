062a0afaf31eb767b9702e174667964aa741bf9c
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-25 23:32:58 +0300

    Filter the displayed images

--- src/components/Album/index.tsx
+++ src/components/Album/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import StackGrid, { transitions } from 'react-stack-grid';
+import StackGrid, { transitions, easings } from 'react-stack-grid';
 import styled from 'styled-components';
 
 // Import components
@@ -12,8 +12,12 @@ import type { Photo } from '../../types';
 import { useAppContext } from '../../state/store';
 import { usePrevious } from '../../hooks/usePrevious';
 
+// Import helper function
+import { getFilteredPhotos } from './helper';
+
 // Extract scaleDown from transitions;
 const { scaleDown } = transitions;
+const { quartOut } = easings;
 
 const StyledStackGrid = styled(StackGrid)`
   margin: 50px auto;
@@ -24,7 +28,7 @@ type Props = {
   photos: Photo[];
 };
 
-const Album = ({ photos }: Props) => {
+const Album = ({ photos = [] }: Props) => {
   const grid = useRef();
   // Use context
   const { state } = useAppContext();
@@ -32,13 +36,22 @@ const Album = ({ photos }: Props) => {
   const prevState = usePrevious(state);
 
   // Destract needed state values
-  const { gutters, columnWidth, isRandomSize } = state;
+  const { gutters, columnWidth, isRandomSize, isRealImages, searchingValue } =
+    state;
 
   useEffect(() => {
-    if (state !== prevState && grid && grid.current) {
+    if (
+      (state.gutters !== prevState?.gutters ||
+        state.columnWidth !== prevState?.columnWidth ||
+        state.isRandomSize !== prevState?.isRandomSize ||
+        state.isRealImages !== prevState?.isRealImages ||
+        state.searchingValue !== prevState?.searchingValue) &&
+      grid &&
+      grid.current
+    ) {
       grid.current.updateLayout();
     }
-  }, [state, prevState, grid]);
+  }, [state, prevState]);
 
   return (
     <StyledStackGrid
@@ -52,8 +65,10 @@ const Album = ({ photos }: Props) => {
       gutterWidth={gutters}
       gutterHeight={gutters}
       monitorImagesLoaded={isRandomSize}
+      duration={480}
+      easing={quartOut}
     >
-      {photos.map((photo: Photo) => (
+      {getFilteredPhotos(searchingValue, photos).map((photo: Photo) => (
         <PhotoImage key={photo.id} photo={photo} />
       ))}
     </StyledStackGrid>
