467c8983e4a3ce67b6a591a57b58d4009e96f914
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-25 14:39:15 +0300

    Create the initial page and display the account switcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,53 @@
-const HomePage = () => {
+import { GetServerSideProps, NextPage } from 'next';
+import styled from 'styled-components';
+
+// Import components
+import AccountSwitch from '../components/AccountSwitcher';
+
+// Import types
+import { User } from '../types';
+
+// Import API
+import { getUsers } from '../utils/api';
+
+type Props = {
+  users: User[];
+};
+
+const Container = styled.section`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-context: center;
+  flex-direction: column;
+  padding: 50px;
+`;
+
+const Header = styled.h2`
+  font-weight: bold;
+  color: #757171;
+`;
+
+const Desc = styled.span`
+  color: #a0a0a0;
+  padding-top: 10px;
+`;
+
+const HomePage: NextPage<Props> = ({ users = [] }) => {
   return (
-    <section>
-      <span>Pavlos Koutoglou</span>
-    </section>
+    <Container>
+      <Header>Welcome to TheLook</Header>
+      <Desc>Please select an account to discover user photos.</Desc>
+      <AccountSwitch accounts={users} />
+    </Container>
   );
 };
 
 export default HomePage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const users = await getUsers();
+
+  // Pass data to the page via props
+  return { props: { users: users } };
+};
