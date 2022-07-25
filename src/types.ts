6ed214c92ba6ee4d4d6f5116409e8297dbc96806
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-26 00:49:22 +0300

    Remove unused types

--- src/types.ts
+++ src/types.ts
@@ -58,47 +58,6 @@ export type Photo = {
 };
 
 /**
- * The response type of errors from /api/*.
- */
-export type ErrorResponse = string;
-
-/**
- * The response type of /users
- */
-// export type UsersResponse = {
-//   results: User[];
-// };
-
-/**
- * The response type of /users.
- */
-export type UsersResponse = User[];
-
-/**
- * The response type of /api/albums
- */
-export type AlbumsResponse = {
-  results: Album[];
-};
-
-/**
- * The response type of /api/album/[id].
- */
-export type AlbumResponse = Album;
-
-/**
- * The response type of /api/photos
- */
-export type PhotosResponse = {
-  results: Photo[];
-};
-
-/**
- * The response type of /api/photos/[id].
- */
-export type PhotoResponse = Photo;
-
-/**
  * This type user for state management
  */
 
