3ad42d2788e53afffc0465642e7bc49211230674
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-25 15:11:36 +0300

    Create photo image component

---
+++ src/components/PhotoImage/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
+
+// Import components
+import ImageGenerator from '../ImageGenerator';
+
+// Import types
+import { Photo } from '../../types';
+
+// Import utils
+import { generateRandom } from '../../utils/random';
+
+// Import store
+import { useAppContext } from '../../state/store';
+
+const StyledImageGenerator = styled(ImageGenerator)`
+  border-radius: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+`;
+
+type Props = {
+  photo: Photo;
+};
+
+const PhotoImage = ({ photo: { thumbnailUrl, title } }: Props) => {
+  // Use context
+  const { state } = useAppContext();
+
+  // Destract needed state values
+  const { isRealImages, columnWidth, isRandomSize } = state;
+
+  if (isRealImages) {
+    return (
+      <StyledImageGenerator
+        title={title}
+        width={columnWidth}
+        height={isRandomSize ? generateRandom(100, 500, 40) : columnWidth}
+        random
+      />
+    );
+  }
+
+  return <img alt={thumbnailUrl} src={thumbnailUrl} width={150} height={150} />;
+};
+
+export default PhotoImage;
