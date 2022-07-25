f3d1fdfdc4341b2c1bb6b87e53ad99da506b7b7e
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-25 15:08:42 +0300

    Change layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,29 @@
+import styled from 'styled-components';
+
+// Import components
 import Header from '../Header';
 import Footer from '../Footer';
+import Content from '../Content';
+import UserSettings from '../UserSettings';
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => (
-  <main>
+  <StyledMain>
     <Header />
-    {children}
+    <Content>{children}</Content>
     <Footer />
-  </main>
+    <UserSettings />
+  </StyledMain>
 );
 
 export default Layout;
